f065f9249beff3d37d1445ab1b1348ae94163960
Emmanuel De Vera <124873866+devsdevera@users.noreply.github.com>
2023-08-31 23:37:46 +1200

    Added a javascript scroll to the about section

--- javascript/smooth.js
+++ javascript/smooth.js
@@ -6,9 +6,19 @@ var btn = document.querySelector('.back-to-top');
 // Add an event listener to the button
 btn.addEventListener('click', function () {
 // Use the smooth scroll behavior to smoothly scroll to the top of the page
-window.scroll({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
 });
+
+// Add an event listener to the button
+document.querySelector('a[href="#about"]').addEventListener('click', function (event) {
+// Use the smooth scroll behavior to smoothly scroll to the top of the page
+    window.scroll({
+        top: 1640,
+        left: 0,
+        behavior: 'smooth'
+    });
 });
