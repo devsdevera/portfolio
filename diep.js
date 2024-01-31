015c169fcb532ed03e371925bc836585292a1168
Emmanuel De Vera <124873866+devsdevera@users.noreply.github.com>
2024-01-31 17:36:39 +1300

    removing additive to chart height

--- diep.js
+++ diep.js
@@ -621,7 +621,7 @@ class Chart {
     this.points = 0;
     
     this.chartWidth = W;
-    this.chartHeight = H + 50;
+    this.chartHeight = H;
     this.statHeight = H / 8.0;
     this.statWidth = W - 60;
     
